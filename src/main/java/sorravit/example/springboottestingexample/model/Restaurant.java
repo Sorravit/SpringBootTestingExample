047a29178cce82189bcc6fdd474d7f846d4499c6
Big <sorravit16758@gmail.com>
2025-08-21 18:54:38 +0700

    Add constructor

--- src/main/java/sorravit/example/springboottestingexample/model/Restaurant.java
+++ src/main/java/sorravit/example/springboottestingexample/model/Restaurant.java
@@ -1,9 +1,11 @@
 package sorravit.example.springboottestingexample.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Restaurant {
     private Long id;
 
